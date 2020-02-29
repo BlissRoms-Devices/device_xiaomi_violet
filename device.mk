3383604a0dc2e475633c4149e4c7040a63d5382b
PIPIPIG233666 <pig.priv@gmail.com>
2020-02-29 13:13:18 -0500

    Violet: Exclude SystemUI overlay from RRO
    Change-Id: I38db9519b8daa50adc12fe48bb2e73d6f87e840b

--- device.mk
+++ device.mk
@@ -42,6 +42,10 @@ PRODUCT_CHARACTERISTICS := nosdcard
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay/frameworks/base/packages/SystemUI
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
